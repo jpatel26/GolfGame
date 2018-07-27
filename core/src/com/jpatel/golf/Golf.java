3a7abd73c742340939fcd180c101084b5dbf970f
John Bradbury <johnbradbury39@gmail.com>
2018-07-27 13:38:40 -0700

    initial screens

--- core/src/com/jpatel/golf/Golf.java
+++ core/src/com/jpatel/golf/Golf.java
@@ -1,33 +1,44 @@
 package com.jpatel.golf;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Golf extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Golf extends Game {
+  
+  public SpriteBatch batch;
+  public BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		//Use LibGDX's default Arial font.
+    font = new BitmapFont();
+		
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
+	  super.render();
+	  /*
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(img, 10, 10, 50, 50);
 		batch.end();
+		*/
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
--- desktop/src/com/jpatel/golf/desktop/DesktopLauncher.java
+++ desktop/src/com/jpatel/golf/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.jpatel.golf.Golf;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = true;
+		config.title = "Golf";
 		new LwjglApplication(new Golf(), config);
 	}
 }
